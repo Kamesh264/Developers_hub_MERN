44e763000546e4c4fbd969e6fa35f77919b8c7fb
Kamesh264 <narsimhakamesh101@gmail.com>
2024-05-22 14:03:37 +0530

    Updated files

--- server/middleware.js
+++ server/middleware.js
@@ -1,47 +1,47 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// module.exports = function(req, res, next){
-//     try{
-//         let token = req.header('x-token');
-//         if(!token){
-//             return res.status(400).send('Token not found');
-//         }
-//         let decoded = jwt.verify(token,'jwtPassword');
-//         req.user = decoded.user;
-//         next();
-//     }
-//     catch(err){
-//         console.log(err);
-//         return res.status(400).send('Authentication Error');
-//     }
-// }
+module.exports = function(req, res, next){
+    try{
+        let token = req.header('x-token');
+        if(!token){
+            return res.status(400).send('Token not found');
+        }
+        let decoded = jwt.verify(token,'jwtPassword');
+        req.user = decoded.user;
+        next();
+    }
+    catch(err){
+        console.log(err);
+        return res.status(400).send('Authentication Error');
+    }
+}
 
 // /middleware.js
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export async function middleware(request) {
-    const token = request.headers.get('x-token');
+// export async function middleware(request) {
+//     const token = request.headers.get('x-token');
 
-    if (!token) {
-        return NextResponse.json({ message: 'Token not found' }, { status: 400 });
-    }
+//     if (!token) {
+//         return NextResponse.json({ message: 'Token not found' }, { status: 400 });
+//     }
 
-    const response = await fetch(new URL('/api/verify-token', request.url).toString(), {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token }),
-    });
+//     const response = await fetch(new URL('/api/verify-token', request.url).toString(), {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ token }),
+//     });
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        return NextResponse.json({ message: errorData.message }, { status: 400 });
-    }
+//     if (!response.ok) {
+//         const errorData = await response.json();
+//         return NextResponse.json({ message: errorData.message }, { status: 400 });
+//     }
 
-    const data = await response.json();
-    request.user = data.user;
+//     const data = await response.json();
+//     request.user = data.user;
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
