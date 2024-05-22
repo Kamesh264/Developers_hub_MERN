9ca81416113e4de9bbf4cdd20e3860e9a0d14d65
Kamesh264 <narsimhakamesh101@gmail.com>
2024-05-22 11:38:38 +0530

    Updated files in root directory

--- client/src/App.js
+++ client/src/App.js
@@ -24,13 +24,14 @@
 
 // export default App;
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
 import Myprofile from './Myprofile';
 import Indprofile from './Indprofile';
+
 
 const App = () => {
   return (
