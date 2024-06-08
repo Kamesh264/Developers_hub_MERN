4caa42e12dd74ee1f3941a03df088edad687fe70
Kamesh264 <narsimhakamesh101@gmail.com>
2024-06-08 15:10:59 +0530

    Files updated

--- index.js
+++ index.js
@@ -123,4 +123,4 @@ app.get('/myreview',middleware,async(req,res) => {
     }
 })
 
-app.listen(port, ()=> console.log(`Example app listening on port ${port}`))
+app.listen(port, ()=> console.log(`App listening on port ${port}`))
