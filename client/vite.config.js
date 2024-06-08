2129add7c84f391d59e6cc31425ccbc8da9571d8
Kamesh264 <narsimhakamesh101@gmail.com>
2024-06-08 16:39:39 +0530

    Files updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,25 @@
+// vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
 
-
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/css/app.css',
-            './src/Home.js', // Update the entry module here
-        ]),
-    ],
-});
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'client\build\index.html'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
