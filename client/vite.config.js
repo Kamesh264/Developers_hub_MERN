4720baff21fcaecb606a44f1432beb29707e00f8
Kamesh264 <narsimhakamesh101@gmail.com>
2024-06-09 14:03:05 +0530

    Files updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
+  build: {
+    
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      input: resolve(__dirname, 'client/build/index.html'),
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'React',
+        },
       },
     },
   },
-  plugins: [react()],
-  server: {
-    host: "127.0.0.1",
-    port: 3000,
-  },
-});
+})
