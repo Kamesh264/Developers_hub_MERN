c0245e4c79f71715a5b3979b4b90f64bb46d6d24
Kamesh264 <narsimhakamesh101@gmail.com>
2024-06-10 11:50:12 +0530

    server.js updated

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "https://your-frontend.com"
+      "https://dancing-sunshine-62bbee.netlify.app/"
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
