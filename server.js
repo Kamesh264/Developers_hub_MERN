ffa02d87c81317cce8b770bd6ba437451e13964f
Kamesh264 <narsimhakamesh101@gmail.com>
2024-06-10 11:29:33 +0530

    server.js updated

--- server.js
+++ server.js
@@ -30,13 +30,15 @@ app.get('*', (req, res) => {
 });
 
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://dancing-sunshine-62bbee.netlify.app/', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
 
-app.use(cors(corsOptions))
+
 
 
 // app.get('/', (req,res) => {
